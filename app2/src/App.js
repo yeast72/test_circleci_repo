6579b10a12822eb1dcfc4e52ec76bee606d1f6d9
yeast72 <yeast72@gmail.com>
2019-08-22 11:38:15 +0700

    grtwe

--- app2/src/App.js
+++ app2/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React4234
+          Learn React5
         </a>
       </header>
     </div>
