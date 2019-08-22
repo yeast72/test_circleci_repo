cc57f500974a4c6710d17c28eeb06b82a4368456
yeast72 <yeast72@gmail.com>
2019-08-22 11:49:08 +0700

    test app1 build

--- app1/src/App.js
+++ app1/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React App 123466532
+          React App 123466532432
         </a>
       </header>
     </div>
