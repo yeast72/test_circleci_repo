7cbd6038353c33df28cab74a06242d25ebeb7959
yeast72 <yeast72@gmail.com>
2019-08-22 11:48:19 +0700

    change file sh

---
+++ .circleci/trigger_job.sh
@@ -0,0 +1,21 @@
+projects_inc_dep=(`cat projects`)
+echo -e "Calculating dependencies\n"
+for dir in `ls -d */`; do
+  for dep in `go list -f '{{ .Deps }}' ./${dir} 2>/dev/null`; do
+    for project_dep in `echo $dep | grep github.com/tufin/$CIRCLE_PROJECT_REPONAME | egrep -v "vendor|${dir%\/}"`; do
+      if [[ " ${projects_inc_dep[@]} " =~ " ${project_dep##*\/} " ]] && ! [[ " ${projects_inc_dep[@]} " =~ " ${dir%\/} " ]]; then
+        projects_inc_dep+=(${dir%\/})
+      fi
+    done
+  done
+done
+echo -e "Building: ${projects_inc_dep[@]}\n"
+for project in ${projects_inc_dep[@]}; do
+  if grep -Fxq $project project-dirs; then
+    printf "\nTriggerring build for project: "$project
+    curl -s -u ${CIRCLE_TOKEN}: \
+      -d build_parameters[CIRCLE_JOB]=${project} \
+      --data revision=$CIRCLE_SHA1 \
+      https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH
+  fi
+done
